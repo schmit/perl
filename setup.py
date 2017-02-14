67e035ac09616abffc163cc7512390d21b4d3290
Sven Schmit <schmit@stanford.edu>
2017-02-14 13:12:55 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='perl',
+      version='0.1',
+      description='Pure exploration for RL',
+      url='http://github.com/schmit/perl',
+      author='Carlos Riquelme, Sven Schmit',
+      author_email='schmit@stanford.edu',
+      license='MIT',
+      packages=['perl'],
+      zip_safe=False)
