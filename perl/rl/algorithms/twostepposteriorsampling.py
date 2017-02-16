9aa2cf4ba2c95ec56c4efdd4ec1026041ad78eef
Carlos Riquelme <rikel@Carloss-MacBook-Pro-3.local>
2017-02-16 13:08:23 -0800

    Added Decoupled Version of TSPS

--- perl/rl/algorithms/twostepposteriorsampling.py
+++ perl/rl/algorithms/twostepposteriorsampling.py
@@ -47,4 +47,57 @@ class TwoStepPosteriorSampling(PosteriorSampling):
 
 
     def __repr__(self):
-        return "Two step Posterior Sampling"
+        return "Two Step Posterior Sampling"
+
+
+class TwoStepDecoupledPosteriorSampling(PosteriorSampling):
+    def __init__(self, env, p_reward=lambda: Normal(0, 1, 1), threshold=1.0, capacity=10):
+        self.env = env
+        self.sampler, self.posterior = create_sampler(env, p_reward)
+        self.threshold = threshold
+        self.buffer = RingBuffer(capacity)
+
+        self._updated_policy = False
+
+    def init_episode(self, max_tries=15):
+
+        # half the time, use the first policy
+        if random.random() < 0.5:
+            mdp = self.sampler()
+            value, policy = value_iteration(mdp, epsilon=1e-3)
+            self.policy = policy
+            return None
+
+        num_tries = 0
+        # otherwise, find alternative policy
+        while num_tries < max_tries:
+            mdp_1 = self.sampler()
+            mdp_2 = self.sampler()
+            value_1, policy_1 = value_iteration(mdp_1, epsilon=1e-3)
+            value_2, policy_2 = value_iteration(mdp_2, epsilon=1e-3)
+            
+            opt_value_1 = env_value(self.env, value_1)
+            opt_value_2 = env_value(self.env, value_2)
+            alt_value_1 = env_value(self.env, policy_iteration(mdp_1, policy_2))
+            alt_value_2 = env_value(self.env, policy_iteration(mdp_2, policy_1))
+
+            z = opt_value_1 - alt_value_1 + opt_value_2 - alt_value_2
+            if z > max(self.buffer, default=0) or z >= self.threshold * statistics.median(self.buffer):
+                self.buffer.add(z)
+                self.policy = policy_1 if random.random() < 0.5 else policy_2
+                return None
+            num_tries += 1
+
+            # add Z score to buffer
+            self.buffer.add(z)
+
+        # if search failed
+        mdp = self.sampler()
+        value, policy = value_iteration(mdp, epsilon=1e-3)
+        self.policy = policy
+        return None
+
+
+    def __repr__(self):
+        return "Two Step Decoupled Posterior Sampling"
+
