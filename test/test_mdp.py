cd7782c91a90ba1f09f3f52e943c2f0adfaa63d0
Sven Schmit <schmit@stanford.edu>
2017-02-17 14:40:14 -0800

    Added most_duplicates to Sequence MDP and tests

--- test/test_mdp.py
+++ test/test_mdp.py
@@ -7,7 +7,7 @@ numberline = Numberline()
 
 def test_find_all_states():
     all_states = find_all_states(numberline)
-    assert set(range(-4, 5)) == all_states
+    assert list(range(-4, 5)) == all_states
 
 
 
--- test/test_numberline.py
+++ test/test_numberline.py
@@ -7,7 +7,3 @@ numberline = Numberline()
 def test_actions():
     assert numberline.actions(0) == [-1, 1]
 
-def test_transitions():
-    assert numberline.transitions(0, 1) == [(0.6, 1, 0), (0.4, 0, 0)]
-    assert numberline.transitions(4, 1) == [(0.6, None, 1), (0.4, 4, 0)]
-    assert numberline.transitions(-4, -1) == [(0.6, None, 0), (0.4, -4, 0)]
