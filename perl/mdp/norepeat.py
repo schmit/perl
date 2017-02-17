c5a3e923492cbefa0877fc78fa81889e7d3cdd14
Carlos Riquelme <rikel@Carloss-MacBook-Pro-3.local>
2017-02-16 16:11:19 -0800

    Added new MDP: norepeat.

--- perl/mdp/norepeat.py
+++ perl/mdp/norepeat.py
@@ -13,14 +13,16 @@ def Norepeat(n=5, discount=1, p_success=0.9):
 
     def transitions(state, action):
         assert action in range(1, n+1)
-        # what happens if action succeeds
+
+        def condition_reward(state, n):
+            return 1 if set(state) == set(range(n+1)) else 0
 
         if len(state) == n+1:
             # after collecting n numbers, game is over
             return [(1, (None, 0))]
 
         next_state = tuple(state[:] + [action])
-        next_reward = 1 if set(next_state) == set(range(n+1)) else 0
+        next_reward = condition_reward(next_state, n)
         trans = [(p_success, (next_state, next_reward))]
         eps = (1-p_success)/(n-1)
 
@@ -28,7 +30,7 @@ def Norepeat(n=5, discount=1, p_success=0.9):
             if num == action:
                 continue
             num_state = tuple(state[:] + [num])
-            num_reward = 1 if set(num_state) == set(range(n+1)) else 0
+            num_reward = condition_reward(num_state, n)
             trans.append((eps, (num_state, num_reward)))
 
         return trans
